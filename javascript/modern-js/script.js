f70d7e8cd40ad3259b6c71b5f4a5e58a8a124b7f
99Jeferson <jefersonodong1@gmail.com>
2026-05-11 22:12:40 +0300

    Refactor code for consistency and clarity, including variable naming and formatting improvements

--- javascript/modern-js/script.js
+++ javascript/modern-js/script.js
@@ -1,64 +1,81 @@
-// VARIABLES 
-let age = 22
+// VARIABLES
+let age = 22;
+
 const country = "Uganda";
 
 console.log(age);
 console.log(country);
 
+
 // TEMPLATE LITERALS
 const username = "Jeferson";
-const role = "Frontend Developer";
+const myRole = "Frontend Developer";
 
-const intro = `My name is ${username} and I am a ${role}`;
+const intro = `My name is ${username} and I am a ${myRole}`;
+
 console.log(intro);
 
-// ARROW FUNCTIONS
+
+// ARROW FUNCTION
 const welcome = () => {
     console.log("Welcome Developer");
 };
 
 welcome();
 
+
 // ARRAYS
 const languages = ["HTML", "CSS", "JavaScript"];
+
 console.log(languages);
 
 languages.push("React");
+
 console.log(languages);
 
+
 // LOOPING
-languages.forEach(language => {
+languages.forEach((language) => {
     console.log(language);
 });
+
 
 // OBJECTS
 const developer = {
     name: "Jeferson",
     age: 22,
     country: "Uganda",
-    skills: ["HTML", "CSS", "JavaScript"],
-    role: "Frontend Developer"
+    skills: ["HTML", "CSS", "JavaScript"]
 };
+
 console.log(developer);
+
 console.log(developer.name);
 
-//DESTRUCTURING 
-const {name, skills, role} = developer;
+
+// DESTRUCTURING
+const { name, skills } = developer;
+
 console.log(name);
+
 console.log(skills);
-console.log(role);
+
 
 // SPREAD OPERATOR
-const forntendskills = ["HTML", "CSS"];
-const allskills = [...frontendskills, "JavaScript", "React"];
-console.log(allskills);
+const frontendSkills = ["HTML", "CSS"];
+
+const allSkills = [...frontendSkills, "JavaScript", "React"];
+
+console.log(allSkills);
+
 
 // FETCH API
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((users) => {
-        
-    users.forEach(user => {
-        console.log(user.name);
-    });
+
+        users.forEach((user) => {
+            console.log(user.name);
+        });
+
     });
