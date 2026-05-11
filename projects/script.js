58c62825cdc9939ca83493ba572168e4f27691c2
99Jeferson <jefersonodong1@gmail.com>
2026-05-11 13:13:12 +0300

    removing what is not required for this webpage

--- projects/script.js
+++ projects/script.js
@@ -3,7 +3,7 @@ const message = document.getElementById("message");
 const title = document.getElementById("title");
 
 button.addEventListener("click", function () {
-    message.textContent = "Hello! Welcome to my developer journey 🚀";
+    message.textContent = "Hello! Welcome to my developer journey ";
     message.style.color = "green";
 
     title.textContent = "You just interacted with the DOM!";
