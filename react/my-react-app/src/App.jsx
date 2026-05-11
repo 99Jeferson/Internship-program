cf8002992186668da4d9faac23d92f30da47059c
99Jeferson <jefersonodong1@gmail.com>
2026-05-11 23:45:53 +0300

    feat: add Welcome component and update introductory text in App component

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,11 +1,17 @@
+function Welcome() {
+  return <h2>Welcome to React Development 🚀</h2>;
+}
+
 function App() {
   return (
     <div>
       <h1>My First React App</h1>
 
       <p>
-        Getting to know React better and interacting with it's components and features is really fun! I am excited to learn more and build amazing applications with React. 
+        I am learning ReactJS during my internship.
       </p>
+
+      <Welcome />
     </div>
   );
 }
